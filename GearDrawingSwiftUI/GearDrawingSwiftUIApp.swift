ec043ca5cdaea2ad8c7dcdf7b9d6351a27b4ca86
Axel Ancona Esselmann <axel@anconaesselmann.com>
2021-09-17 13:24:40 -0700

    first commit

---
+++ GearDrawingSwiftUI/GearDrawingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GearDrawingSwiftUIApp.swift
+//  GearDrawingSwiftUI
+//
+//  Created by Axel Ancona Esselmann on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct GearDrawingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
